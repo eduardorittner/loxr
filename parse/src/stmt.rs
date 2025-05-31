fa89de965b532a4a212348754e8ec8ff002864ce
eduardorittner <eduardorittnerc@gmail.com>
2025-05-31 12:59:55 -0300

    Added trailing semicolon parsing in expressions

--- parse/src/stmt.rs
+++ parse/src/stmt.rs
@@ -11,9 +11,13 @@ where
         match self.peek() {
             Some(T!['{']) => self.block(),
             Some(T![print]) => self.print(),
-            _ => ast::Stmt::Expr {
-                expr: self.expression(),
-            },
+            _ => {
+                let expr = ast::Stmt::Expr {
+                    expr: self.expression(),
+                };
+                self.consume(T![;]);
+                expr
+            }
         }
     }
 
@@ -40,19 +44,75 @@ where
 #[cfg(test)]
 mod tests {
     use crate::Parser;
-    use ast;
+    use ast::*;
+
+    fn test_stmt(source: &str, expected: ast::Stmt) {
+        let mut parser = Parser::new(source);
+
+        let stmt = parser.statement();
+        assert_eq!(stmt, expected)
+    }
 
     #[test]
     fn test_print_stmt() {
         let source = "print ident;";
-        let mut parser = Parser::new(source);
 
-        let stmt = parser.statement();
-        assert_eq!(
-            stmt,
+        test_stmt(
+            source,
             ast::Stmt::Print {
-                expr: ast::Expr::Ident("ident".to_string())
-            }
-        )
+                expr: ast::Expr::Ident("ident".to_string()),
+            },
+        );
+    }
+
+    #[test]
+    fn test_expr_stmt() {
+        test_stmt(
+            "1 + 2;",
+            ast::Stmt::Expr {
+                expr: Expr::BinaryOp {
+                    op: BinaryOp::Add,
+                    rhs: Box::new(Expr::Literal(Lit::Number(2.))),
+                    lhs: Box::new(Expr::Literal(Lit::Number(1.))),
+                },
+            },
+        );
+
+        test_stmt(
+            "27;",
+            ast::Stmt::Expr {
+                expr: Expr::Literal(Lit::Number(27.)),
+            },
+        );
+
+        test_stmt(
+            "\"I am a string\";",
+            ast::Stmt::Expr {
+                expr: Expr::Literal(Lit::Str("I am a string".to_string())),
+            },
+        );
+
+        test_stmt(
+            "false;",
+            ast::Stmt::Expr {
+                expr: Expr::Literal(Lit::Bool(false)),
+            },
+        );
+
+        test_stmt(
+            "true;",
+            ast::Stmt::Expr {
+                expr: Expr::Literal(Lit::Bool(true)),
+            },
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_expr_stmt_without_semicolon() {
+        let source = "1 + 2";
+
+        let mut parser = Parser::new(source);
+        parser.statement();
     }
 }
